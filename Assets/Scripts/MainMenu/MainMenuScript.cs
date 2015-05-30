58321f2b1e1c9887c67c9a54c0e15af40ef6cdb6
Raul <raulnguyen91@gmail.com>
2015-05-30 15:35:22 +0200

    - main menu v0.00001 added

---
+++ Assets/Scripts/MainMenu/MainMenuScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void StartButtonPressed()
+    {
+        Debug.Log("Start Button Pressed");
+    }
+
+    public void OptionButtonPressed()
+    {
+        Debug.Log("Option Button Pressed");
+    }
+
+    public void ExitButtonPressed()
+    {
+        Debug.Log("Exit Button Pressed");
+    }
+}
