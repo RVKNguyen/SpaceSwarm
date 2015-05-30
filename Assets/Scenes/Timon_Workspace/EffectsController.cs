60360bbfa48b0e2674fcc7e404c066417e6776fc
TheUnimake <heratum@hotmail.de>
2015-05-30 16:25:50 +0200

    added Timon's test level + scripts

---
+++ Assets/Scenes/Timon_Workspace/EffectsController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Effect : MonoBehaviour {
+	void Start() {
+		shipController = gameObject.GetComponent<ShipController> ();
+	}
+
+	public void Update() {
+
+
+	}
+
+	protected ShipController shipController;
+	public float MaxLifeTime;
+	public float CurrentLifeTime;
+}
+
+public class DamageEffect : Effect {
+
+
+	void Update() {
+
+
+
+	}
+}
+
+public class EffectsController : MonoBehaviour {
+	List<Effect> Effects;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < Effects.Count;) {
+			Effects[i].Update();
+
+			Effects[i].CurrentLifeTime += Time.deltaTime;
+
+			if (Effects[i].CurrentLifeTime > Effects[i].MaxLifeTime) {
+				Effects.RemoveAt(i);
+			} else {
+				i++;
+			}
+		}
+	
+	}
+}
---
+++ Assets/Scenes/Timon_Workspace/Movement.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float Speed = 1F;
+
+	private GameObject Head;
+
+	// Use this for initialization
+	void Start () {
+		int i = 0;
+
+		for (float z = 0F; z <= 20F; z += 2F) {
+			var obj = Object.Instantiate(GameObject.Find ("Cube"));
+			obj.transform.position += new Vector3(0F, 0F, z);
+			obj.name = "Karl " + i.ToString(); 
+
+				i++;
+		}
+
+		Head = GameObject.Find ("Head");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPosition = gameObject.transform.position + Head.transform.forward * Speed * Time.deltaTime;
+
+		if (IsInsideLevel (newPosition)) {
+			gameObject.transform.position = newPosition;
+		}
+
+		RaycastHit myhit = new RaycastHit();
+		Ray myray = new Ray();
+		myray.origin = gameObject.transform.position;
+		myray.direction = Head.transform.forward;
+
+		if (Physics.Raycast(myray, out myhit, 1000.0f))
+			Debug.Log(myhit.collider.name);
+	}
+
+	bool IsInsideLevel(Vector3 newPos) {
+		if (Mathf.Abs (newPos.x) > 10F) {
+			return false;
+		}
+
+		if (Mathf.Abs (newPos.y) > 10F) {
+			return false;
+		}
+
+		return true;
+	}
+}
