011c3af7a08171264685eb14152d95096d686bba
Raul <raulnguyen91@gmail.com>
2015-05-30 20:06:35 +0200

    updated MainMenu scene

---
+++ Assets/Scripts/Misc/rotate_z.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate_z : MonoBehaviour {
+    
+    public float speed;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.right * speed * Time.deltaTime);
+	}
+}
