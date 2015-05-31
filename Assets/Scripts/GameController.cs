931a4a85130213a48c6410545c98cf79d2ab5bd5
Raul <raulnguyen91@gmail.com>
2015-05-31 08:22:02 +0200

    auto shooting implemented

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -15,7 +15,7 @@ public class GameController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-       
+        
 	}
 
     IEnumerator SpawnAsteroids()
--- Assets/Scripts/Player/MovementController.cs
+++ Assets/Scripts/Player/MovementController.cs
@@ -10,6 +10,12 @@ public class MovementController : MonoBehaviour {
     public float boundaryMinY;
     public float boundaryMaxY;
 
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private float nextFire;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -17,6 +23,12 @@ public class MovementController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        Movement();
+        Aiming();
+	}
+
+    void Movement()
+    {
         float moveHorizontal = transform.FindChild("Head").transform.forward.x;
         float moveVertical = transform.FindChild("Head").transform.forward.y;
 
@@ -31,5 +43,31 @@ public class MovementController : MonoBehaviour {
         );
 
         GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
-	}
+    }
+
+    void Aiming()
+    {
+        RaycastHit hit;
+        Camera camera = GetComponent<Camera>();
+        var cameraCenter = camera.ScreenToWorldPoint(new Vector3(Screen.width / 2f, Screen.height / 2f, camera.nearClipPlane));
+
+        if (Physics.Raycast(cameraCenter, this.transform.forward, out hit, 10000))
+        {
+            var obj = hit.transform.gameObject;
+            if (obj.tag == "Enemy")
+            {
+                //Debug.Log("Shoot!");
+                Shoot();
+            }
+        }
+    }
+    void Shoot()
+    {
+        if (Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            GetComponent<AudioSource>().Play();
+        }
+    }
 }
