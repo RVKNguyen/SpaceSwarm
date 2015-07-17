6649c5923a482bc1b0656f1a718a05d2d9792383
SandraKriemann <sandra.kriemann@gmail.com>
2015-07-17 21:50:03 +0200

    tried sound switcher

--- Assets/Scripts/Options/SoundButtonScript.cs
+++ Assets/Scripts/Options/SoundButtonScript.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class SoundButtonScript : MonoBehaviour {
+    public GameObject explosion;
     public static bool soundOn;
     public AudioSource audioSource;
     public AudioClip soundOnOffClip;
@@ -12,8 +13,11 @@ public class SoundButtonScript : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
-        soundOn = true;
-        clone = (Instantiate(tick, new Vector3(1.8F, 3.12F, 7.98F), Quaternion.identity) as Transform).gameObject;
+        if (GameObject.Find("check(Clone)") == null)
+        {
+            soundOn = true;
+            clone = (Instantiate(tick, new Vector3(1.8F, 3.12F, 7.98F), Quaternion.identity) as Transform).gameObject;
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -24,17 +28,20 @@ public class SoundButtonScript : MonoBehaviour {
         {
             audioSource.Play();
             Destroy(clone, 0.0F);
+            Debug.Log("soundOn is true");
             soundOn = false;
         }
         else
         {
             trans = Instantiate(tick, new Vector3(1.8F, 3.12F, 7.98F), Quaternion.identity) as Transform;
             clone = trans.gameObject;
+            Debug.Log("soundOn is false");
             soundOn = true;
         }
 
         if (gameObject.name == "btn_Sound")
         {
+            Instantiate(explosion, transform.position, transform.rotation);
             Destroy(gameObject);
         }
     }
