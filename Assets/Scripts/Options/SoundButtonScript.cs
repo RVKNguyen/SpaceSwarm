ca4577323ffd2e31e1cc43462c5c956eda13d609
SandraKriemann <sandra.kriemann@gmail.com>
2015-07-17 15:34:30 +0200

    adjusted some sounds, added frame for options menue

---
+++ Assets/Scripts/Options/SoundButtonScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundButtonScript : MonoBehaviour {
+    public AudioSource audioSource;
+    public AudioClip soundOnOffClip;
+
+    void OnTriggerEnter(Collider other)
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = soundOnOffClip;
+        audioSource.Play();
+
+
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
