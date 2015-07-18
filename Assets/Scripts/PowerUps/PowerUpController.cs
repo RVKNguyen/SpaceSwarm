346861adafb07e04e8e3df2e55da63e533e2d0ce
Raul <raulnguyen91@gmail.com>
2015-07-18 22:37:54 +0200

    collecting power ups implemented: add life, add points

--- Assets/Scripts/PowerUps/PowerUpController.cs
+++ Assets/Scripts/PowerUps/PowerUpController.cs
@@ -6,14 +6,27 @@ public class PowerUpController : MonoBehaviour {
     public float speed;
     public string type;
 
+    private GameController gameController;
+
 	// Use this for initialization
 	void Start () {
         GetComponent<Rigidbody>().velocity = transform.forward * speed;
+
+        GameObject gameControllerObject = GameObject.FindGameObjectWithTag("EventSystem");
+        //Debug.Log(gameControllerObject);
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    
 	}
 
     void OnTriggerEnter(Collider other)
@@ -25,8 +38,20 @@ public class PowerUpController : MonoBehaviour {
 
         if (other.tag == "Player")
         {
-            Debug.Log("PowerUp Collected: " + type);
-
+            switch (type)
+            {
+                case "addLife":
+                    Debug.Log("PowerUp Collected: " + type);
+                    gameController.UpdateLife(100);
+                    break;
+                case "addPoints":
+                    Debug.Log("PowerUp Collected: " + type);
+                    gameController.UpdateScore(100);
+                    break;
+                default:
+                    Debug.Log("PowerUp Fail!");
+                    break;
+            }
 
 
 
